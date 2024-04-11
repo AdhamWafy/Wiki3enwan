e9fce52360b2b4de95085be15348ebdea6eb6e12
AdhamWafy <144845319+AdhamWafy@users.noreply.github.com>
2024-04-11 21:05:42 +0200

    Update sitemap.py

--- Wiki3enwan/sitemaps/sitemap.py
+++ Wiki3enwan/sitemaps/sitemap.py
@@ -13,4 +13,4 @@ class enwaanSitemap(Sitemap):
 
     def location(self, item):
         # Construct the URL for each user-added page using reverse()
-        return reverse('enwaan-detail', kwargs={'slug': item.slug})
+        return reverse(item.slug, kwargs={'slug': item.slug})
